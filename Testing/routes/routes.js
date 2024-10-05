e24bd98fbc706a91d9dfc0cbe134ddaae7c68a87
Sath Don <s220105594@deakin.edu.au>
2024-10-05 11:54:13 +1000

    Delete Testing directory

--- Testing/routes/routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { handleAddOrder, handleGetOrders } = require('../controllers/controllers');  
-
-const router = express.Router();
-
-router.post('/addOrder', handleAddOrder);
-router.get('/getOrders', handleGetOrders);
-
-module.exports = router;
